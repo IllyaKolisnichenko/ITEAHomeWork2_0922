fe7f05816a97493cd5caf22eaffa888f7b162acb
Denis Smereka <den.smereka@mail.ru>
2022-10-31 21:27:12 +0200

    fix

--- homework_2.cpp
+++ homework_2.cpp
@@ -13,18 +13,18 @@ void Swap(int* Values, int left, int right)
 
 int main(int argc, char** argv)
 {
-
-    int Values[10]{};
+    const int size_of_array{10};
+    int Values[size_of_array]{};
     for (int i = 0; i < argc-1; i++)
     {
         *(Values+i) = atoi(argv[i+1]);
     }
 
     bool swapped = true;
-    while (swapped != false)
+    while (swapped)
     {
         swapped = false;
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i < size_of_array; i++) {
             if (*(Values+i-1) > *(Values+i))
             {
                 Swap(Values, i-1, i);
@@ -32,7 +32,7 @@ int main(int argc, char** argv)
             }
         }
     }
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < size_of_array; i++)
     {
         std::cout << i+1 << " element = " <<  Values[i] << std::endl;
     }
