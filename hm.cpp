bd7dea2ecd2f9889f0fc95fe7f802e873213c738
danilpocht <danapoctamcev@gmail.com>
2022-10-31 19:08:24 +0200

    feat: home work

---
+++ hm.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <locale>
+
+void Sort(int*, int);
+
+void Sort(int* array, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int y = i + 1; y < size; y++)
+        {
+            if (*(array + y) < *(array + i))
+            {
+                std::swap(*(array + i), *(array + y));
+            }
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+
+
+    const int size= 10;
+    int array[size] {};
+
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << "введите " << i + 1 << " число: ";
+        std::cin >> *(array + i);
+    }
+
+    Sort(array, size);
+
+    std::cout << "Результат";
+    for (auto element : array)
+    {
+        std::cout << element << " ";
+    }
+}
+
