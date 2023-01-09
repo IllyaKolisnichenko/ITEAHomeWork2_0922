ad328dbb3946dfa5a93ba783b7d0c1f12b1450ce
root <root@DESKTOP-QLFAEBA.localdomain>
2023-01-09 21:37:01 +0200

    homework part2(fixed)

--- hw2.cpp
+++ hw2.cpp
@@ -4,44 +4,42 @@ using namespace std;
 void Sort(int* array, int size)
 {
     int x;
-    for (int i=1; i<size; i++) 
+    for (int i = 1; i < size; i++)
     {
-        for (int j=size-1; j>=i; j--) 
+        for (int j = size - 1; j >= i; j--)
         {
-            if (*(array+j)<*(array+j-1)) 
+            if (*(array + j) < *(array + j - 1))
             {
-                
-                x = *(array+j-1) ; 
-                
-                *(array+j-1) = *(array+j) ;
-                
-                *(array+j) = x ;
+
+                x = *(array + j - 1);
+
+                *(array + j - 1) = *(array + j);
+
+                *(array + j) = x;
             }
         }
     }
 }
 
-int main(){
-int size = 10;
-int array[size];
-    
-for (int i = 0; i < size; i++)
+int main()
 {
-   
-   cout << "Number of element: " << i+1 <<"   ";
-   cout << "Enter element: ";  
-   cin >> *(array+i);
-      
-} 
+    int size = 10;
+    int array[size];
 
-Sort(array,size);
-cout<<endl;
-cout<<"Result after bubble sort: " << endl;
-for (int i = 0; i < size; i++)
-{
-  
-    cout<< *(array+i)<< endl;
+    for (int i = 0; i < size; i++)
+    {
+
+        cout << "Number of element: " << i + 1 << "   ";
+        cout << "Enter element: ";
+        cin >> *(array + i);
+    }
+
+    Sort(array, size);
+    cout << endl;
+    cout << "Result after bubble sort: " << endl;
+    for (int i = 0; i < size; i++)
+    {
+
+        cout << *(array + i) << endl;
+    }
 }
-
-}
-
